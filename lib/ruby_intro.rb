0ec32bd1efa3f9bcf447c0ced526ce225fe91ac1
Benjamin Ortner <bortner@gmail.com>
2018-08-29 12:50:29 +0000

    complete part 1 and 2 of first part

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,12 +3,31 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
+
+
+# Define a method `max_2_sum(array)` which takes an array of integers 
+# as an argument and returns the sum of its two largest elements. For 
+# an empty array it should return zero. For an array with just one 
+# element, it should return that element. Run associated tests via:
+# `$ rspec spec/part1_spec.rb:23`
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.size <= 2 then
+	sum arr
+  else
+	arr = arr.sort.reverse
+	arr[0] + arr[1]
+  end 
 end
+
+
+# Define a method `sum_to_n?(array, n)` that takes an array of integers and an 
+# additional integer, n, as arguments and returns true if any two elements in the 
+# array of integers sum to n. `sum_to_n?([], n)` should return false for any value 
+# of n, by definition. Run associated tests via:  `$ rspec spec/part1_spec.rb:42`
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
